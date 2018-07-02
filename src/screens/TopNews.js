1059ffa79a621486d36666fd695704350880e26d
Francis Breidenbach <fbreidenbach@gmail.com>
2018-07-02 14:13:49 -0700

    Add styling to FlatList and its Items.

--- src/screens/TopNews.js
+++ src/screens/TopNews.js
@@ -25,7 +25,7 @@ export default class TopNews extends Component {
 
   renderItem = ({ item }) => (
     <TouchableOpacity>
-      <View style={{ flex: 1, padding: 5 }}>
+      <View style={styles.listItem}>
         <Text>{item.title}</Text>
       </View>
     </TouchableOpacity>
@@ -37,7 +37,22 @@ export default class TopNews extends Component {
         data={this.state.articles}
         keyExtractor={this.keyExtractor}
         renderItem={this.renderItem}
+        style={styles.list}
       />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  list: {
+    marginTop: 60,
+    borderTopColor: "#f2f2f2",
+    borderTopWidth: 1
+  },
+  listItem: {
+    borderBottomColor: "#f2f2f2",
+    borderBottomWidth: 1,
+    flex: 1,
+    padding: 5
+  }
+});
